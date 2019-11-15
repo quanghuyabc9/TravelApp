84a2994e312931ff0b58057b7350f8d421c903e7
= <nguyenanhhao.hcmus@gmail.com>
2019-11-16 03:48:58 +0700

    add BottomActionBar and get TourList fragment

--- app/src/main/java/com/example/travelguide/view/MainActivity.java
+++ app/src/main/java/com/example/travelguide/view/MainActivity.java
@@ -1,81 +1,75 @@
 package com.example.travelguide.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.os.Handler;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.travelguide.R;
-import com.example.travelguide.manager.MyApplication;
-import com.example.travelguide.network.MyAPIClient;
-import com.example.travelguide.network.UserService;
+import com.example.travelguide.view.RecyclerDataAdapter;
+import com.example.travelguide.view.TourItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ProgressDialog mProgressDialog;
-    private UserService userService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mProgressDialog = new ProgressDialog(this);
-        mProgressDialog.setCancelable(false);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmet_container, new ListTourFragment()).commit();
 
-        MyApplication app = (MyApplication) getApplication();
-        TextView tv = (TextView) findViewById(R.id.textView);
-        //tv.setText(getString(R.string.hello) + " " + app.getTokenInfo().getUserName());
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment = null;
+                switch (menuItem.getItemId()){
+                    case R.id.nav_list_tour:
+                        selectedFragment = new ListTourFragment();
+                        break;
+                    case R.id.nav_history:
+                        selectedFragment = new HistoryFragment();
+                        break;
+                    case R.id.nav_map:
+                        selectedFragment = new MapFragment();
+                        break;
+                    case R.id.nav_noti:
+                        selectedFragment = new NotificationsFragment();
+                        break;
+                    case R.id.nav_setting:
+                        selectedFragment = new SettingsFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmet_container, selectedFragment).commit();
+                return true;
+            }
+        });
 
-        userService = MyAPIClient.getInstance().getAdapter().create(UserService.class);
-        Button logout = (Button) findViewById(R.id.b_Logout);
-//            @Override
-//            public void onClick(View view) {
-//                mProgressDialog.show();
-//                Call<Void> call = userService.logout();
-//                call.enqueue(new Callback<Void>() {
-//                    @Override
-//                    public void onResponse(Call<Void> call, Response<Void> response) {
-//                        // Clear token
-//                        MyAPIClient.getInstance().setAccessToken(null);
-//                        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(getString(R.string.shared_pref_name), 0);
-//                        SharedPreferences.Editor editor = sharedPref.edit();
-//                        editor.remove(MainActivity.this.getString(R.string.saved_access_token));
-//                        editor.remove(MainActivity.this.getString(R.string.saved_access_token_time));
-//                        editor.commit();
-//                        // Open LoginActivity
-//                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-//                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//                        //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | IntentCompat.FLAG_ACTIVITY_CLEAR_TASK);
-//                        mProgressDialog.hide();
-//                        MainActivity.this.startActivity(intent);
-//                        MainActivity.this.finish();
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<Void> call, Throwable t) {
-//                        mProgressDialog.hide();
-//                    }
-//                });
-//            }
-//        });
-
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        if(mProgressDialog != null)
-//            mProgressDialog.dismiss();
-//    }
-//
     }
+
 }
---
+++ app/src/main/java/com/example/travelguide/view/TourItem.java
@@ -0,0 +1,54 @@
+package com.example.travelguide.view;
+
+public class TourItem {
+    private Integer img_src;
+    private String location;
+    private String date;
+    private String quantity;
+    private String price;
+
+    public TourItem(Integer img_src, String location, String date, String quantity, String price){
+        this.img_src = img_src;
+        this.location = location;
+        this.date = date;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public void setImg_src(Integer img_src){
+        this.img_src = img_src;
+    }
+    public void setLocation(String location){
+        this.location = location;
+    }
+    public void setDate(String date){
+        this.date = date;
+    }
+    public void setQuantity(String quantity){
+        this.quantity = quantity;
+    }
+    public void setPrice(String price){
+        this.price = price;
+    }
+
+    public Integer getImg_src() {
+        return img_src;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+}
+
