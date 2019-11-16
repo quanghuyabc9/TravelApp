b8c8fb370358508804648d2e0b3e6fb03f53ab41
quanghuyabc9 <quanghuyabc9@gmail.com>
2019-11-16 22:52:03 +0700

    Added Sing In with Facebook feature, get AccesToken successfully

--- app/src/main/java/com/example/travelguide/view/LoginActivity.java
+++ app/src/main/java/com/example/travelguide/view/LoginActivity.java
@@ -4,10 +4,14 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.TokenWatcher;
 import android.text.TextUtils;
+import android.util.Base64;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -24,19 +28,38 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.travelguide.R;
+import com.example.travelguide.manager.Constants;
 import com.example.travelguide.manager.MyApplication;
 import com.example.travelguide.model.LoginRequest;
 import com.example.travelguide.model.LoginResponse;
 import com.example.travelguide.network.MyAPIClient;
 import com.example.travelguide.network.UserService;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.tasks.Task;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Date;
 
 import retrofit2.Call;
@@ -46,6 +69,7 @@ import retrofit2.Response;
 public class LoginActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 0;
     public static String TAG  = "LoginActivity";
+    private static final String EMAIL = "email";
 
     // UI references.
     private AutoCompleteTextView emailPhoneView;
@@ -65,16 +89,41 @@ public class LoginActivity extends AppCompatActivity {
             relLayout_SignUpForgotPwBtn.setVisibility(View.VISIBLE);
         }
     };
+    private CallbackManager callbackManager;
+    private LoginButton signInButton_Facebook;
 
+    public static void printHashKey(Context pContext) {
+        try {
+            PackageInfo info = pContext.getPackageManager().getPackageInfo(pContext.getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String hashKey = new String(Base64.encode(md.digest(), 0));
+                Log.i(TAG, "printHashKey() Hash Key: " + hashKey);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, "printHashKey()", e);
+        } catch (Exception e) {
+            Log.e(TAG, "printHashKey()", e);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        //printHashKey(this);
+        // Google service for signing in
+        // Configure sign-in to request offline access to the user's ID, basic
+        // profile, and Google Drive. The first time you request a code you will
+        // be able to exchange it for an access token and refresh token, which
+        // you should store. In subsequent calls, the code will only result in
+        // an access token. By asking for profile access (through
+        // DEFAULT_SIGN_IN) you will also get an ID Token as a result of the
+        // code exchange.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestScopes(new Scope(Scopes.DRIVE_APPFOLDER))
+                .requestServerAuthCode(Constants.Google_ClientID)
                 .requestEmail()
                 .build();
 
@@ -92,6 +141,36 @@ public class LoginActivity extends AppCompatActivity {
                         break;
                     // ...
                 }
+            }
+        });
+
+        //Facebook service for signing in
+        callbackManager = CallbackManager.Factory.create();
+
+        signInButton_Facebook = findViewById(R.id.signInButton_Facebook);
+        signInButton_Facebook.setPermissions(Arrays.asList(EMAIL));
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        signInButton_Facebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Toast.makeText(LoginActivity.this, "Sign in with Facebook success", Toast.LENGTH_LONG).show();
+                Log.i(TAG, "quanghuy said: " + loginResult.getAccessToken().toString());
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Toast.makeText(LoginActivity.this, "Sign in with Facebook cancelled", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Toast.makeText(LoginActivity.this, "Sign in with Facebook error", Toast.LENGTH_LONG).show();
+                Log.i(TAG, exception.getMessage());
             }
         });
 
@@ -145,10 +224,10 @@ public class LoginActivity extends AppCompatActivity {
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             // The Task returned from this call is always completed, no need to attach
@@ -156,19 +235,22 @@ public class LoginActivity extends AppCompatActivity {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
         }
+        else {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
+            //String test = account.getServerAuthCode();
             // Signed in successfully, show authenticated UI.
-           // updateUI(account);
+            updateUI(account);
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-           // updateUI(null);
+            updateUI(null);
         }
     }
 
@@ -190,13 +272,46 @@ public class LoginActivity extends AppCompatActivity {
     private void updateUI(GoogleSignInAccount account) {
         //Change UI according to user data.
         if (account != null) {
-            Toast.makeText(this, "U Signed In successfully", Toast.LENGTH_LONG).show();
-            Toast.makeText(this, account.getEmail(), Toast.LENGTH_LONG).show();
-            String tokenid = account.getIdToken();
+            Toast.makeText(this, "Sign in with Google success", Toast.LENGTH_LONG).show();
+            //Toast.makeText(this, account.getEmail(), Toast.LENGTH_LONG).show();
+
+            String test = account.getServerAuthCode();
+            OkHttpClient client = new OkHttpClient();
+            RequestBody requestBody = new FormEncodingBuilder()
+                    .add("grant_type", "authorization_code")
+                    .add("client_id", Constants.Google_ClientID)
+                    .add("client_secret", Constants.Google_ClientSecret)
+                    .add("redirect_uri","")
+                    .add("code", account.getServerAuthCode())
+                    .build();
+            final Request request = new Request.Builder()
+                    .url("https://www.googleapis.com/oauth2/v4/token")
+                    .post(requestBody)
+                    .build();
+            client.newCall(request).enqueue(new com.squareup.okhttp.Callback() {
+                        @Override
+                        public void onFailure(Request request, IOException e) {
+                            Log.e(TAG, e.toString());
+                        }
+
+                        @Override
+                        public void onResponse(com.squareup.okhttp.Response response) throws IOException {
+                            try {
+                                JSONObject jsonObject = new JSONObject(response.body().string());
+                                final String message = jsonObject.toString(5);
+                                String accessToken = jsonObject.getString("access_token");
+                                Log.i(TAG, message);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+            );
+
 
             //startActivity(new Intent(this,AnotherActivity.class));
         } else {
-            Toast.makeText(this, "U Didnt signed in", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Sign in with Google error", Toast.LENGTH_LONG).show();
         }
     }
 
