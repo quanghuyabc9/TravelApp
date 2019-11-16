8b7f75abcb53ab1a1663da862b69092fcfd54506
quanghuyabc9 <quanghuyabc9@gmail.com>
2019-11-16 11:31:33 +0700

    Added Google Sign In Button, hasn't completed Sign In by Google feature

--- app/src/main/java/com/example/travelguide/view/LoginActivity.java
+++ app/src/main/java/com/example/travelguide/view/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.TokenWatcher;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -18,6 +19,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,6 +29,13 @@ import com.example.travelguide.model.LoginRequest;
 import com.example.travelguide.model.LoginResponse;
 import com.example.travelguide.network.MyAPIClient;
 import com.example.travelguide.network.UserService;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import java.util.Date;
 
@@ -35,6 +44,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 0;
     public static String TAG  = "LoginActivity";
 
     // UI references.
@@ -43,6 +53,9 @@ public class LoginActivity extends AppCompatActivity {
     private UserService userService;
     private RelativeLayout relLayout_SignInFormWithAppName, relLayout_SignUpForgotPwBtn;
     private ProgressDialog progressDialog;
+    private GoogleSignInClient mGoogleSignInClient;
+    private SignInButton signInButton_Google;
+
 
     Handler handler = new Handler();
     Runnable runnable = new Runnable() {
@@ -58,6 +71,29 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton_Google = findViewById(R.id.signInButton_Google);
+        signInButton_Google.setSize(SignInButton.SIZE_WIDE);
+        signInButton_Google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.signInButton_Google:
+                        signIn();
+                        break;
+                    // ...
+                }
+            }
+        });
 
         relLayout_SignInFormWithAppName = findViewById(R.id.relLayout_SignInFromWithAppName);
         relLayout_SignUpForgotPwBtn = findViewById(R.id.relLayout_SignInForgotPwBtn);
@@ -104,10 +140,66 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+           // updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+           // updateUI(null);
+        }
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
 }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        // GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        // updateUI(account);
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        //Change UI according to user data.
+        if (account != null) {
+            Toast.makeText(this, "U Signed In successfully", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, account.getEmail(), Toast.LENGTH_LONG).show();
+            String tokenid = account.getIdToken();
+
+            //startActivity(new Intent(this,AnotherActivity.class));
+        } else {
+            Toast.makeText(this, "U Didnt signed in", Toast.LENGTH_LONG).show();
+        }
+    }
+
 
     /**
      * Attempts to sign in or register the account specified by the login form.
